59089517a2aa8970185c72887a97fd9b798a2702
Heardline <Kuznetsovjobstr@gmail.com>
2021-07-25 22:22:41 +0300

    Переделал уведомления за 10 минут до начала

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
 
 from numpy import exp
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
@@ -11,7 +12,7 @@ from sqlalchemy.orm import sessionmaker
 import aioschedule
 
 from config import db,Auth
-from commands import register_commands,register_callbacks
+from commands import register_commands,register_callbacks,get_users,get_next_lesson
 from utils.db.base import Base
 
 #pdb.update_data()
@@ -19,11 +20,30 @@ API_TOKEN = Auth.API_TOKEN
 # Команды для подсказок телеграма
 async def set_bot_commands(bot: Bot):
     commands = [
-        BotCommand(command="today", description="Расписание на сегодня"),
-        BotCommand(command="tomorow", description="Расписаниие на завтра"),
-        BotCommand(command="week", description="Расписание на этой неделе")
+        BotCommand(command="today", description="Пары на сегодня"),
+        BotCommand(command="tomorow", description="Пары на завтра"),
+        BotCommand(command="week", description="Пары на этой неделе"),
+        BotCommand(command="task", description="Мой задачи"),
+        BotCommand(command="addtask", description="Добавить задачу"),
+        BotCommand(command="sub", description="Настроить уведомления")
     ]
     await bot.set_my_commands(commands)
+
+
+async def scheduler():
+    aioschedule.every().day.at("8:20").do(alert_lesson)
+    aioschedule.every().day.at("10:30").do(notif_every_lesson)
+    aioschedule.every().day.at("12:30").do(notif_every_lesson)
+    aioschedule.every().day.at("14:10").do(notif_every_lesson)
+    aioschedule.every().day.at("16:10").do(notif_every_lesson)
+    aioschedule.every().day.at("17:50").do(notif_every_lesson)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(5)
+
+bot = Bot(API_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot,storage=MemoryStorage())
+
 # Логи + SQLAlchemy + aiogram + polling
 async def main():
     logging.basicConfig(
@@ -39,18 +59,16 @@ async def main():
 
     # expire_on_commit=False will prevent attributes from being expired
     # after commit.
+    asyncio.create_task(scheduler())
     async_session = sessionmaker(
         engine, expire_on_commit=False, class_=AsyncSession
     )
-    bot = Bot(API_TOKEN, parse_mode="HTML")
     bot["db"] = async_session
-    dp = Dispatcher(bot,storage=MemoryStorage())
 
     register_commands(dp)
     register_callbacks(dp)
-
     await set_bot_commands(bot)
-    
+
     try:
         await dp.start_polling()
     finally:
@@ -58,36 +76,13 @@ async def main():
         await dp.storage.wait_closed()
         await bot.session.close()
 
-# Уведомлялки утром
-'''async def notif_morning():
-    for user in UsersDB.find({"sub":"True"}):
-        #try:
-        group = UsersDB.find_one({"chat_id":user["chat_id"]})["group"]
-        Lesson = "<b> Доброе утро! </b> \n "
-        a = 0 #Локальная переменная - формирует адрес исходя из четной/нечетной неделе и когда.
-        if time_lesson.NumberOfMonth() % 2 == 0:
-            a = 1
-        if pdb.get_lesson(time_lesson.todayIs()+1+a,group) == "nan":
-            Lesson = Lesson + " <code> Ты везунчик, можешь немного поспать, к первой не надо. </code> \n"
-            if pdb.get_lesson(time_lesson.todayIs()+3+a,group) == "nan":
-                Lesson = Lesson + "Ко второй тоже. Вообще топ.\n"
-        a = 0
-        Lesson = Lesson + "Сегодня у тебя: \n"
-        check_lesson = False
-        for i in range(1,7):
-            if time_lesson.NumberOfMonth() % 2 == 0: 
-                 a = i*2
-            else:
-                a = (i*2)-1
-             # Четная/ не четная неделя
-            if pdb.get_lesson(time_lesson.todayIs()+a,group) == "nan":
-                pass
-            else: 
-                check_lesson = True
-                Lesson = pdb.ready_lesson(Lesson,group,a,i)
-        if check_lesson is False:
-            Lesson = "<b>Сегодня нету пар </b> ✨🎉\n Спи спокойно и иди гуляй)"       
-        await bot.send_message(user["chat_id"], Lesson, parse_mode='HTML', disable_web_page_preview=True)
+# Уведомлялки за 10 минут до пары
+async def alert_lesson():
+    students = await get_users(bot)
+    for student in students:
+        lesson = await get_next_lesson(bot,student.telegram_id)
+        if lesson:
+            await bot.send_message(student.telegram_id,f'💬 Через 10 минут начнется {lesson.name} {lesson.room} {lesson.type}',disable_notification=False)
 
 # Уведомлять после каждой пары
 async def notif_every_lesson():
@@ -109,24 +104,9 @@ async def notif_every_lesson():
         else: 
             Lesson = pdb.ready_lesson(Lesson,group,a,now)     
         await bot.send_message(user["chat_id"], Lesson, parse_mode='HTML', disable_web_page_preview=True)
-       
-async def scheduler():
-    aioschedule.every().day.at("8:40").do(notif_morning)
-    aioschedule.every().day.at("10:30").do(notif_every_lesson)
-    aioschedule.every().day.at("12:30").do(notif_every_lesson)
-    aioschedule.every().day.at("14:10").do(notif_every_lesson)
-    aioschedule.every().day.at("16:10").do(notif_every_lesson)
-    aioschedule.every().day.at("17:50").do(notif_every_lesson)
-    while True:
-        await aioschedule.run_pending()
-        await asyncio.sleep(5)
-
-async def on_startup(x):
-    asyncio.create_task(scheduler())
-'''
+    
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         logging.error("Bot stopped!")   
-
