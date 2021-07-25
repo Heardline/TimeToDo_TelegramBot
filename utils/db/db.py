518862a66ff9f7c21bebfabefe1000b31fb1bacf
Heardline <Kuznetsovjobstr@gmail.com>
2021-07-25 19:33:31 +0300

    Ревью кода и правки

--- utils/db/db.py
+++ utils/db/db.py
@@ -5,7 +5,7 @@ from sys import intern
 import pandas as pd,os
 import enum
 from sqlalchemy.sql.expression import column
-from sqlalchemy import select,insert
+from sqlalchemy import select,insert,delete
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.traversals import CACHE_IN_PLACE
 import utils.time_lessons as time_lesson
@@ -40,6 +40,7 @@ class Task(Base):
     student = Column('student', ForeignKey('student.telegram_id'))
     lesson = Column('lesson',String)
     time = Column('day',DateTime)
+    status = Column('status', String)
 class Group(Base):
     __tablename__ = "group"
     __table_args__ = {'extend_existing': True}
@@ -63,12 +64,15 @@ async def import_group(message,group,univercity):
         print(group)
 
 async def import_lesson(message,name,teacher,room,type,time, group):
+    # Да да да тут if, if чтобы избавится от косяков преподавателей составляющих расписание.
+    # Мне было просто в падлу case использовать.
     if room != room:
         room = ''
     if teacher != teacher:
         teacher = ''
     if type != type:
         type = ''
+    # Определяет четность недели
     if time%2 == 1:
         odd = 0
     else:
@@ -79,12 +83,28 @@ async def import_lesson(message,name,teacher,room,type,time, group):
         session.add(lesson)
         await session.commit()
 
+async def remove_data(message):
+    db_session = message.bot.get('db')
+    async with db_session() as session:
+        sql = delete(Lesson)
+        await session.execute(sql)
+        await session.commit()
 
 async def import_from_xlsx(message):
     excel_names = os.listdir("data/xlsx/")
     await message.answer('Запускаю процесс обновления данных...')
+    # Очищаем данные чтобы небыло дублей
+    await remove_data(message)
     for names in excel_names:
         data_df = pd.read_excel("data/xlsx/"+names,header=1)
+        # Процесс парсинга построин так:
+        # - Так как расписание в вертикальном виде, index у нас будут содержать названия групп.
+        # - Массивом пробегаемся по индексам и берем найденные группы содержащие '-'
+        # - Импортируем в таблицу группы
+        # - Таблица в ширину содержит 75 строк. Первые 12 строк - это понедельник, 1 строка - это первая пара нечетной недели понедельника, 2 строка первая пара четной,
+        # 13 строка - это первая пара вторника и так мы берем пары, деля на 12. 
+        # Теперь iloc-ом пытаемся вытащить название пары. в параметры пишим [time() - строка, get_loc(индекс) - колонка)
+        # - Преподавателей и т.д. Берем прибавляем +1 к колонке в зависимости в какой колонке расположены данные. То есть на сколько справо от пары.
         for index in data_df.columns:
             if "-" in str(index):
                 await import_group(message,str(index),names.split(r'_')[0])
@@ -94,4 +114,5 @@ async def import_from_xlsx(message):
                     else:
                         await import_lesson(message,data_df.iloc[time,data_df.columns.get_loc(index)],data_df.iloc[time,data_df.columns.get_loc(index)+2],data_df.iloc[time,data_df.columns.get_loc(index)+3],data_df.iloc[time,data_df.columns.get_loc(index)+1],time,index) 
     await message.answer("Обновление данных завершено.")
+        # Пример как он получает данные. Эта команда выдаст первую пару четной недели у группы ГДБО
         #print(data_df.iloc[2,data_df.columns.get_loc("ГДБО-01-20")])
--- utils/scrap_schedul.py
+++ utils/scrap_schedul.py
@@ -2,6 +2,7 @@ import requests, re
 import pandas as pd
 from bs4 import BeautifulSoup
 
+## Функция парсинга расписания с сайта Mirea 
 def get_schedul():
     headers = {
         "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36"
@@ -10,17 +11,20 @@ def get_schedul():
     r = requests.get(url=url,headers=headers)
 
     soup = BeautifulSoup(r.text,'lxml')
+    ## Получаем список ссылок на расписания. У нас они содержится в карточках uk-link-toggle. 
     scheduler_cards = soup.find_all('a', class_="uk-link-toggle")
 
     schedul_urls = []
     for schedul in scheduler_cards:
         schedul_url = schedul.get('href')
+        ## Ищет названия расписаний, в которых не содержится экз и т.д.
         if re.search(r'"Экз|экз|Экз_',schedul_url) or re.search(r'Зач|зач|сессия|Колледж|РТУ',schedul_url) or re.search(r'pdf|doc',schedul_url) :
             pass
         else:
             schedul_urls.append(schedul_url)
-    return schedul_urls
+    return schedul_urls ## Обязательно передать обратно список ссылок.
 
+## Отдельная функция. Тоже самое что и сверху, только берет экзамены. Сделал на будущие
 def get_schedul_exam():
     headers = {
         "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36"
@@ -38,6 +42,7 @@ def get_schedul_exam():
             schedul_urls.append(schedul_url)
     return schedul_urls
 
+## Можно не сохранять, однако я решил это делать, чтобы отслеживать изменения
 def download_xlsx():
     for schedul_url in get_schedul():
         resp = requests.get(schedul_url)
