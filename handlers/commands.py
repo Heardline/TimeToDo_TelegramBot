81fa7a6324ee1adfdfde95312510c4c3efa8789d
Heardline <Kuznetsovjobstr@gmail.com>
2021-08-22 12:50:06 +0300

    Исправления команд

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,20 +1,19 @@
 
-from admin import update_data
+from handlers.admin import update_data
 import utils.time_lessons as time_lesson
 from aiogram import Dispatcher, types
-from sqlalchemy import and_, select
-from utils.db.db import Lesson, Student, import_from_xlsx
-from utils.simple_calendar import calendar_callback
 
-from menu import menu
-from register import (
-    RegisterState, select_group, select_notify, send_welcome
+from utils.simple_calendar import calendar_callback
+from utils.states import TaskState,RegisterState
+from handlers.menu import menu
+from handlers.register import (
+    select_group, select_notify, send_welcome
 )
-from schedul_lesson import (
+from handlers.schedul_lesson import (
     scheduler_today, scheduler_tomorrow, scheduler_week
 )
-from task import (
-    TaskState, process_simple_calendar, task, task_add,
+from handlers.task import (
+    process_simple_calendar, task, task_add,
     task_add_handler, task_callback, task_delete, task_done,
     task_name, task_select
 )
@@ -33,7 +32,7 @@ def register_commands(dp: Dispatcher):
     #dp.register_message_handler( , )
 
     # Расписание
-    dp.register_message_handler(scheduler_today,commands='day'),
+    dp.register_message_handler(scheduler_today,commands='today'),
     dp.register_message_handler(scheduler_tomorrow,commands='tomorrow'),
     dp.register_message_handler(scheduler_week,commands='week'),
 
@@ -53,24 +52,6 @@ def register_callbacks(dp: Dispatcher):
     dp.register_callback_query_handler(task_add_handler, task_callback.filter(act='add'))
     #dp.register_callback_query_handler(task_update, task_callback.filter(act='update')),
 
-# Поиска студента в БД
-async def get_student(bot):
-    db_session = bot.get('db')
-    async with db_session() as session:
-        sql = select(Student).where(Student.notify == True)
-        request = await session.execute(sql)
-        return request.scalars()
 
-# Передает семинар для оповещения
-async def get_next_lesson(bot,telegram_id):
-    db_session = bot.get('db')
-    async with db_session() as session:
-        student = await get_student(session,telegram_id)
-        group = student.grp
-        if time_lesson.NumberOfMonth() % 2 == 0: week=0 
-        else: week=1 
-        sql = select(Lesson).where(and_(Lesson.grp == group, time_lesson.todayIs(), Lesson.week == week, Lesson.time == time_lesson.convertHourtoLesson))
-        request = await session.execute(sql)
-        return request.scalar()
 
 
