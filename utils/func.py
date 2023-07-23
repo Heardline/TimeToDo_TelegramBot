a7a02ce46528c2f5f374de0498f753027e129cdf
Alexsandr Kuznetsov <38807417+Heardline@users.noreply.github.com>
2023-07-23 14:32:06 +0300

    Рефакторинг func.py
    Небольшие правки и доработки

--- utils/func.py
+++ utils/func.py
@@ -2,29 +2,40 @@ from sqlalchemy import and_, select
 from utils.db.db import Lesson, Student
 import utils.time_lessons as time_lesson
 
-# Поиск студента 
+# Получение сессии базы данных
+def get_db_session(bot):
+    return bot.get('db')
+
+# Поиск студента по ID пользователя в Telegram
 async def get_student(db_session, user_id: int) -> Student:
     sql = select(Student).where(Student.telegram_id == user_id)
     request = await db_session.execute(sql)
-    student: Student = request.scalar()
+    student: Student = request.scalar_one_or_none()
     return student
 
-# Поиска студентов с включенными уведомлениями
-async def get_students(bot):
-    db_session = bot.get('db')
+# Поиск студентов, у которых включены уведомления
+async def get_students_with_notifications(bot):
+    db_session = get_db_session(bot)
     async with db_session() as session:
         sql = select(Student).where(Student.notify == True)
         request = await session.execute(sql)
-        return request.scalars()
+        return request.scalars().all()
 
-# Передает семинар для оповещения
-async def get_next_lesson(bot,telegram_id):
-    db_session = bot.get('db')
+# Получение следующего урока студента
+async def get_next_lesson(bot, telegram_id):
+    db_session = get_db_session(bot)
     async with db_session() as session:
-        student = await get_students(session,telegram_id)
+        student = await get_student(session, telegram_id)
+        if not student:
+            return None
+
         group = student.grp
-        if time_lesson.NumberOfMonth() % 2 == 0: week=0 
-        else: week=1 
-        sql = select(Lesson).where(and_(Lesson.grp == group, time_lesson.todayIs(), Lesson.week == week, Lesson.time == time_lesson.convertHourtoLesson))
+        week = 0 if time_lesson.NumberOfMonth() % 2 == 0 else 1
+        sql = select(Lesson).where(and_(
+            Lesson.grp == group,
+            time_lesson.todayIs(),
+            Lesson.week == week,
+            Lesson.time == time_lesson.convertHourtoLesson
+        ))
         request = await session.execute(sql)
-        return request.scalar()
+        return request.scalar_one_or_none()
