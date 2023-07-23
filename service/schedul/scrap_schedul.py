9d6034549f10044320f3a2e08405d5888d2aff1b
Alexsandr Kuznetsov <38807417+Heardline@users.noreply.github.com>
2023-07-23 14:28:46 +0300

    Рефакторинг scrap_schedul.py
    Убрал лишние функции. Добавил проверки

--- service/schedul/scrap_schedul.py
+++ service/schedul/scrap_schedul.py
@@ -1,50 +1,75 @@
-import requests, re
-import pandas as pd
+import os
+import re
+import requests
 from bs4 import BeautifulSoup
 
-## Функция парсинга расписания с сайта Mirea 
-def get_schedul():
-    headers = {
-        "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36"
-    }
-    url = "https://www.mirea.ru/schedule/"
-    r = requests.get(url=url,headers=headers)
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36"
+}
+URL = "https://www.mirea.ru/schedule/"
 
-    soup = BeautifulSoup(r.text,'lxml')
-    ## Получаем список ссылок на расписания. У нас они содержится в карточках uk-link-toggle. 
+SCHEDULE_PATTERNS = ["Экз", "экз", "Экз_", "Зач", "зач", "сессия", "Колледж", "РТУ", "pdf", "doc"]
+EXAM_PATTERNS = ["Экз", "экз", "Экз_", "Зач", "зач"]
+
+
+def get_schedule_urls(patterns):
+    try:
+        r = requests.get(url=URL, headers=HEADERS)
+        r.raise_for_status()
+    except requests.HTTPError as errh:
+        print("Http Error:", errh)
+        return []
+    except requests.ConnectionError as errc:
+        print("Error Connecting:", errc)
+        return []
+    except requests.Timeout as errt:
+        print("Timeout Error:", errt)
+        return []
+    except requests.RequestException as err:
+        print("Something Else:", err)
+        return []
+
+    soup = BeautifulSoup(r.text, 'lxml')
     scheduler_cards = soup.find_all('a', class_="uk-link-toggle")
 
     schedul_urls = []
     for schedul in scheduler_cards:
         schedul_url = schedul.get('href')
-        ## Ищет названия расписаний, в которых не содержится экз и т.д.
-        if re.search(r'"Экз|экз|Экз_',schedul_url) or re.search(r'Зач|зач|сессия|Колледж|РТУ',schedul_url) or re.search(r'pdf|doc',schedul_url) :
-            pass
-        else:
+        if not any(re.search(pattern, schedul_url) for pattern in patterns):
             schedul_urls.append(schedul_url)
-    return schedul_urls ## Обязательно передать обратно список ссылок.
 
-## Отдельная функция. Тоже самое что и сверху, только берет экзамены. Сделал на будущие
-def get_schedul_exam():
-    headers = {
-        "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36"
-    }
-    url = "https://www.mirea.ru/schedule/"
-    r = requests.get(url=url,headers=headers)
-
-    soup = BeautifulSoup(r.text,'lxml')
-    scheduler_cards = soup.find_all('a', class_="uk-link-toggle")
-
-    schedul_urls = []
-    for schedul in scheduler_cards:
-        schedul_url = schedul.get('href')
-        if re.search(r'"Экз|экз|Экз_',schedul_url) or re.search(r'Зач|зач',schedul_url):
-            schedul_urls.append(schedul_url)
     return schedul_urls
 
-## Можно не сохранять, однако я решил это делать, чтобы отслеживать изменения
-def download_xlsx():
-    for schedul_url in get_schedul():
-        resp = requests.get(schedul_url)
-        with open("data/xlsx/"+schedul_url.split("/")[-1],'wb') as output:
+
+def download_files(output_dir, url_list):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for schedul_url in url_list:
+        try:
+            resp = requests.get(schedul_url)
+            resp.raise_for_status()
+        except requests.HTTPError as errh:
+            print("Http Error:", errh)
+            continue
+        except requests.ConnectionError as errc:
+            print("Error Connecting:", errc)
+            continue
+        except requests.Timeout as errt:
+            print("Timeout Error:", errt)
+            continue
+        except requests.RequestException as err:
+            print("Something Else:", err)
+            continue
+
+        file_name = os.path.join(output_dir, schedul_url.split("/")[-1])
+        with open(file_name, 'wb') as output:
             output.write(resp.content)
+
+
+def main():
+    schedule_urls = get_schedule_urls(SCHEDULE_PATTERNS)
+    download_files("data/xlsx", schedule_urls)
+
+    exam_urls = get_schedule_urls(EXAM_PATTERNS)
+    download_files("data/exam_xlsx", exam_urls)
