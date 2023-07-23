f15122cf563dc6734017362ead1a936bd862ae4e
Alexsandr Kuznetsov <38807417+Heardline@users.noreply.github.com>
2023-07-23 14:35:52 +0300

    Рефакторинг register.py

--- handlers/register.py
+++ handlers/register.py
@@ -1,4 +1,3 @@
-
 from utils.states import RegisterState
 from utils.db.db import Group, Student
 from aiogram import types
@@ -9,55 +8,54 @@ from sqlalchemy import select
 from utils.func import get_student
 from config import Settings
 
-# /start
+# Чтение приветственного сообщения и сообщения группы
+with open(Settings.cmd_welcome, 'r', encoding='utf-8') as file:
+    WELCOME_MESSAGE = file.read()
+with open(Settings.cmd_group, 'r', encoding='utf-8') as file:
+    GROUP_MESSAGE = file.read()
 
+# Получение сессии базы данных
+def get_db_session(bot):
+    return bot.get('db')
+
+# Обработка команды /start
 async def send_welcome(message: types.Message, state: FSMContext):
-    db_session = message.bot.get('db')
-    # Проверяем есть ли в базе студент
+    db_session = get_db_session(message)
     async with db_session() as session:
         student = await get_student(session, message.from_user.id)
         if not student:
-            async with state.proxy() as data:
-                data['telegram_id'] = message.from_user.id
+            # Если студент не найден, начинаем процесс регистрации
+            telegram_id = message.from_user.id
+            await state.update_data(telegram_id=telegram_id)
             await RegisterState.group_select.set()
-            # Спрашиваем группу
-            with open(Settings.cmd_welcome, 'r', encoding='utf-8') as file:
-                await message.reply(file.read(), parse_mode='HTML', disable_web_page_preview=True)
+            await message.reply(WELCOME_MESSAGE, parse_mode='HTML', disable_web_page_preview=True)
         else:
             await message.answer("<b> Привет, рад тебя видеть снова </b>", parse_mode='HTML', disable_web_page_preview=True)
 
-# Внесение группы
-
+# Выбор группы студента
 async def select_group(message: types.Message, state: FSMContext):
-    db_session = message.bot.get('db')
-    sql = select(Group).where(Group.name == message.text)
+    db_session = get_db_session(message)
     async with db_session() as session:
+        sql = select(Group).where(Group.name == message.text)
         request = await session.execute(sql)
-        group = request.scalar()
+        group = request.scalar_one_or_none()
+
         if group:
-            async with state.proxy() as data:
-                data['group'] = message.text
-            markup = types.ReplyKeyboardMarkup(
-                resize_keyboard=True, selective=True)
+            await state.update_data(group=message.text)
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
             markup.add("Да", "Нет")
-            with open(Settings.cmd_group, 'r', encoding='utf-8') as file:
-                await message.answer(file.read(), parse_mode='HTML', disable_web_page_preview=True, reply_markup=markup)
+            await message.answer(GROUP_MESSAGE, parse_mode='HTML', disable_web_page_preview=True, reply_markup=markup)
             await RegisterState.next()
         else:
             await message.reply("Что-то пошло не так, проверь, чтобы группа была формата ГИБО-05-19. Если не получается, значит база данных не доступна либо ваша группа не загружена.", parse_mode='HTML', disable_web_page_preview=True)
 
-# Настройка Уведомление за 20 минут
-
+# Настройка уведомлений за 20 минут
 async def select_notify(message: types.Message, state: FSMContext):
-    db_session = message.bot.get('db')
+    db_session = get_db_session(message)
     async with db_session() as session:
-        async with state.proxy() as data:
-            if message.text == "Да":
-                notify = True
-            else:
-                notify = False
-            student = Student(
-                telegram_id=data['telegram_id'], grp=data['group'], notify=notify)
+        data = await state.get_data()
+        notify = message.text == "Да"
+        student = Student(telegram_id=data['telegram_id'], grp=data['group'], notify=notify)
         session.add(student)
         await session.commit()
     await state.finish()
